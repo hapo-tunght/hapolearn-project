ead62749c0dbb6924e92929d37b0620d22c7d841
hapo-tunght <tung.ht@haposoft.com>
2021-09-14 09:44:56 +0700

    create home blade

---
+++ resources/views/auth/modal.blade.php
@@ -0,0 +1,79 @@
+<!-- Login/Register form -->
+    <div class="login-register-form modal fade p-0" id="modalLogin">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content modal-content-custom border-0">
+                <div class="modal-title">
+                    <ul class="nav nav-tabs w-100">
+                        <li class="nav-item w-50">
+                            <a class="nav-link active" data-toggle="tab" href="#login">LOGIN</a>
+                        </li>
+                        <li class="nav-item w-50">
+                            <a class="nav-link" data-toggle="tab" href="#register">REGISTER</a>
+                        </li>
+                    </ul>
+                    <button type="button" class="close-tab rounded-circle" data-dismiss="modal"><i class="fas fa-times"></i></button>
+                </div>
+                <div class="tab-content">
+                    <div class="tab-pane fade show active" id="login" role="tabpanel"> 
+                        <form class="container ">
+                            <div class="form-group login-username">
+                                <label for="username">Username:</label>
+                                <input type="text" class="form-control" id="username">
+                            </div>
+                            <div class="form-group">
+                                <label for="pwd">Password:</label>
+                                <input type="password" class="form-control" id="pwd">
+                            </div>
+                            <div class="form-group form-check-custom d-flex w-100 align-items-center">
+                                <label class="form-check-label w-50">
+                                    <input class="form-check-input pr-2" type="checkbox">
+                                    <p class="ml-2 font-weight-bold">Remember me</p> 
+                                </label>
+                                <a href="#" class="w-50 mr-3 text-right"><u>Forgot password</u></a>
+                            </div>
+                            <div class="button-cover w-100 d-flex justify-content-center">
+                                <button type="submit" class="btn">LOGIN</button>
+                            </div>                          
+                            <div class="container-fluid login-with d-flex align-items-center p-0 mt-3">
+                                <div class="horizontal-rule"><hr></div>
+                                <p>Login with</p>
+                                <div class="horizontal-rule"><hr></div>
+                            </div>
+                            <div class="login-with-account d-flex flex-column align-items-center">
+                                <a href="#" class="login-with-account-google btn mt-3 d-flex justify-content-center align-items-center">
+                                    <i class="fab fa-google-plus-g mr-2"></i> Google
+                                </a>
+                                <a href="#" class="login-with-account-facebook btn mt-3 mb-5 d-flex justify-content-center align-items-center">
+                                    <i class="fab fa-facebook-f mr-2"></i> Facebook
+                                </a>
+                            </div>                         
+                        </form>
+                    </div>
+                    <div class="tab-pane fade" id="register" role="tabpanel"> 
+                        <form class="container register-form">
+                            <div class="form-group register-username">
+                                <label for="username">Username:</label>
+                                <input type="text" class="form-control" id="username">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" class="form-control" id="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="pwd">Password:</label>
+                                <input type="password" class="form-control" id="pwd">
+                            </div>
+                            <div class="form-group">
+                                <label for="pwd">Repeat Password:</label>
+                                <input type="password" class="form-control" id="pwd">
+                            </div>
+                            <div class="button-cover w-100 d-flex justify-content-center">
+                                <button type="submit" class="btn button-register">REGISTER</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,308 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div id="body" class="body container-fluid position-relative p-0 w-100">
+        <!-- HapoLearn banner -->
+        <div class="hapo-learn-banner container-fluid d-flex align-items-center">         
+            <div class="hapo-learn-banner-content container d-flex flex-column">
+                <span class="hapo-learn-banner-content-title d-block">
+                    Learn Anytime, Anywhere
+                </span>
+                <span class="hapo-learn-banner-content-bold d-block">
+                    at HapoLearn <img src="{{ asset('img/hapo_logo.png') }}" alt="hapo-logo"> !
+                </span>
+                <span class=" hapo-learn-banner-content-quote d-block">
+                    Interactive lessons, "on-the-go" <br> practice, peer support.
+                </span>
+                <a href="#" class="btn d-flex justify-content-center align-items-center" type="button">
+                    Start Learning Now! 
+                </a>
+            </div>           
+        </div>
+        <div class="linear-gradient">
+            <div class="transition-block"></div>
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <!-- Main courses -->
+        <div class="main-courses container-fluid p-0 d-flex justify-content-center ">
+            <div class="row col-lg-10 col-sm-12 mb-0">
+                <div class="main-courses-item col-sm-4 p-sm-0 d-flex justify-content-center">
+                    <div class="main-courses-item-card card h-100 ">
+                        <div class="front-end-image card-header d-flex align-items-center justify-content-center">
+                            <img class="custom-image" src="{{ asset('img/rectangle_7.png') }}" alt="HTML/CSS/js">                          
                         </div>
-                    @endif
+                        <div class="main-courses-item-card-content card-body d-flex flex-column align-items-center">
+                            <p class="card-content-title card-title">HTML/CSS/js Tutorial</p>
+                            <p class="card-content-body card-text">I knew hardly anything about HTML, JS, and CSS before entering New
+                            Media. I had coded quite a bit, but never touched
+                            anything in regards to web development.</p>
+                            <a href="#" class="btn courses-button d-flex justify-content-center align-items-center">Take This Course</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-courses-item col-sm-4 p-sm-0 d-flex justify-content-center">
+                    <div class="main-courses-item-card card h-100">
+                        <div class="laravel-image card-header d-flex align-items-center justify-content-center">
+                            <img class="custom-image" src="{{ asset('img/laravel_1_logo_black_and_white_1.png') }}" alt="laravel">                         
+                        </div>
+                        <div class="main-courses-item-card-content card-body d-flex flex-column align-items-center">
+                            <p class="card-content-title card-title">LARAVEL Tutorial</p>
+                            <p class="card-content-body card-text">I knew hardly anything about HTML, JS, and CSS before entering New
+                            Media. I had coded quite a bit, but never touched
+                            anything in regards to web development.</p>
+                            <a href="#" class="btn courses-button d-flex justify-content-center align-items-center">Take This Course</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-courses-item col-sm-4 p-sm-0 d-flex justify-content-center">
+                    <div class="main-courses-item-card card h-100">
+                        <div class="php-image card-header d-flex align-items-center justify-content-center">
+                            <img class="custom-image" src="{{ asset('img/rectangle_15.png') }}" alt="php">                          
+                        </div>
+                        <div class="main-courses-item-card-content card-body d-flex flex-column align-items-center">
+                            <p class="card-content-title card-title">PHP Tutorial</p>
+                            <p class="card-content-body card-text">I knew hardly anything about HTML, JS, and CSS before entering New
+                            Media. I had coded quite a bit, but never touched
+                            anything in regards to web development.</p>
+                            <a href="#" class="btn courses-button d-flex justify-content-center align-items-center">Take This Course</a>
+                        </div>
+                    </div>
+                </div>
+            </div>           
+        </div>
 
-                    {{ __('You are logged in!') }}
+        <!-- Other Courses -->
+        <div class="other-courses container-fluid p-0">
+            <div class="other-courses-title d-flex justify-content-center">Other courses</div>
+            <div class="other-courses-title-underline mx-auto"></div>
+            <div class="row-cover h-100 d-flex justify-content-center">
+                <div class="row mb-0 col-lg-10 col-sm-12">
+                    <div class="other-courses-item col-sm-4 p-sm-0 d-flex justify-content-center">
+                        <div class="other-courses-item-card card h-100 ">
+                            <div class="css-image cover-image d-flex align-items-center justify-content-center">
+                                <img class="custom-image" src="{{ asset('img/css_course.png') }}" alt="css">
+                            </div>
+                            <div class="other-courses-item-card-content card-body d-flex flex-column align-items-center">
+                                <p class="card-content-title card-title">CSS Tutorial</p>
+                                <p class="card-content-body card-text">I knew hardly anything about HTML, JS, and CSS before entering
+                                    New Media. <br> I had coded quite a bit, but never touched
+                                    anything in regards to web development.</p>
+                                <a href="#" class="btn courses-button d-flex justify-content-center align-items-center">Take This
+                                    Course</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="other-courses-item col-sm-4 p-sm-0 d-flex justify-content-center">
+                        <div class="other-courses-item-card card h-100">
+                            <div class="rails-image cover-image d-flex align-items-center justify-content-center">
+                                <img class="custom-image" src="{{ asset('img/rails_course.png') }}" alt="rails">
+                            </div>
+                            <div class="other-courses-item-card-content card-body d-flex flex-column align-items-center">
+                                <p class="card-content-title card-title">Ruby on rails Tutorial</p>
+                                <p class="card-content-body card-text">I knew hardly anything about HTML, JS, and CSS before entering
+                                    New Media. <br> I had coded quite a bit, but never touched
+                                    anything in regards to web development.</p>
+                                <a href="#" class="btn courses-button d-flex justify-content-center align-items-center">Take This
+                                    Course</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="other-courses-item col-sm-4 p-sm-0 d-flex justify-content-center">
+                        <div class="other-courses-item-card card h-100">
+                            <div class="java-image cover-image d-flex align-items-center justify-content-center">
+                                <img class="custom-image" src="{{ asset('img/java_1.png') }}" alt="java">
+                            </div>
+                            <div class="other-courses-item-card-content card-body d-flex flex-column align-items-center">
+                                <p class="card-content-title card-title">Java Tutorial</p>
+                                <p class="card-content-body card-text">I knew hardly anything about HTML, JS, and CSS before entering
+                                    New Media. <br> I had coded quite a bit, but never touched
+                                    anything in regards to web development.</p>
+                                <a href="#" class="btn courses-button d-flex justify-content-center align-items-center">Take This
+                                    Course</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="view-all-our-courses container d-flex justify-content-center" href="#">
+                View All Our Courses 
+                <span>
+                    <img src="{{ asset('img/right_arrow.png') }}" alt="arrow">
+                </span>
+            </a>
+        </div>
+
+        <!-- Why-hapolearn-banner -->
+        <div class="why-hpl-banner container-fluid p-0">
+            <div class="row w-100 h-100 align-items-center m-0">
+                <div class="why-hpl-text col-sm-6 d-flex flex-column align-items-center pr-0">
+                    <div class="why-hpl-text-title">
+                        <p>Why HapoLearn?</p>
+                    </div>
+                    <div class="text-content">
+                        <i class="fas fa-check-circle"></i>Interactive lessons, "on-the-go" practice, peer support.
+                    </div>
+                    <div class="text-content">
+                        <i class="fas fa-check-circle"></i>Interactive lessons, "on-the-go" practice, peer support.
+                    </div>
+                    <div class="text-content">
+                        <i class="fas fa-check-circle"></i>Interactive lessons, "on-the-go" practice, peer support.
+                    </div>
+                    <div class="text-content">
+                        <i class="fas fa-check-circle"></i>Interactive lessons, "on-the-go" practice, peer support.
+                    </div>
+                    <div class="text-content">
+                        <i class="fas fa-check-circle"></i>Interactive lessons, "on-the-go" practice, peer support.
+                    </div>
+                </div>
+                <div class="multi-device-image col-sm-6 d-flex align-items-center"></div>
+            </div>           
+            <div class="single-device-image"></div>
+
+        </div>
+        <div class="feedback-slider container">
+            <div class="feedback-slider-title d-flex justify-content-center">Feedback</div>
+            <div class="feedback-slider-underline mx-auto"></div>
+            <div class="feedback-slider-content d-flex justify-content-center">
+                <p>What other students turned professionals have to say about us after learning with us and reaching their goals</p>
+            </div>
+            <div class="feedback-slider-comment d-flex">
+                <div class="slide-feedback">
+                    <div class="slide-feedback-comment">
+                        <div class="comment-text">
+                            <p>“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular
+                                developer. Very glad to have taken this course. Thank you Eddie Bryan.”</p>
+                        </div>
+                    </div>
+                    <div class="slide-feedback-user d-flex">
+                        <div class="user-avatar d-flex align-items-center">
+                            <img src="{{ asset('img/info_avavtar.png') }}" alt="avatar">
+                        </div>  
+                        <div class="user-info d-flex flex-column">
+                            <div class="user-info-name">Thanh Tung Hoang</div>
+                            <div class="user-info-lang">PHP</div>
+                            <div class="user-info-vote">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>   
+                        </div>                     
+                    </div>
+                </div>
+                <div class="slide-feedback">
+                    <div class="slide-feedback-comment">
+                        <div class="comment-text">
+                            <p>“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular
+                                developer. Very glad to have taken this course. Thank you Eddie Bryan.”</p>
+                        </div>                       
+                    </div>
+                    <div class="slide-feedback-user d-flex">
+                        <div class="user-avatar d-flex align-items-center">
+                            <img src="{{ asset('img/info_avavtar.png') }}" alt="avatar">
+                        </div>
+                        <div class="user-info d-flex flex-column">
+                            <div class="user-info-name">Anh Hoang Nguyen</div>
+                            <div class="user-info-lang">Java</div>
+                            <div class="user-info-vote">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide-feedback">
+                    <div class="slide-feedback-comment">
+                        <div class="comment-text">
+                            <p>“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular
+                                developer. Very glad to have taken this course. Thank you Eddie Bryan.”</p>
+                        </div>
+                    </div>
+                    <div class="slide-feedback-user d-flex">
+                        <div class="user-avatar d-flex align-items-center">
+                            <img src="{{ asset('img/info_avavtar.png') }}" alt="avatar">
+                        </div>
+                        <div class="user-info d-flex flex-column">
+                            <div class="user-info-name">Nghia Luu Trung</div>
+                            <div class="user-info-lang">PHP</div>
+                            <div class="user-info-vote">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide-feedback">
+                    <div class="slide-feedback-comment">
+                        <div class="comment-text">
+                            <p>“A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular
+                                developer. Very glad to have taken this course. Thank you Eddie Bryan.”</p>
+                        </div>
+                    </div>
+                    <div class="slide-feedback-user d-flex">
+                        <div class="user-avatar d-flex align-items-center">
+                            <img src="{{ asset('img/info_avavtar.png') }}" alt="avatar">
+                        </div>
+                        <div class="user-info d-flex flex-column">
+                            <div class="user-info-name">Tuan Tran Hoang</div>
+                            <div class="user-info-lang">Python</div>
+                            <div class="user-info-vote">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="become-a-member container-fluid d-flex justify-content-center align-items-center ">
+            <div class="become-a-member-content col-md-6 d-flex flex-column align-items-center">
+                <p>Become a member of our growing community!</p>
+                <a class="btn d-flex justify-content-center align-items-center" href="#">Start Learning Now!</a>
+            </div>
+        </div>
+        <div class="statistic container-fluid p-0 d-flex flex-column">
+            <div class="statistic-title d-flex flex-column mx-auto">
+                <p>Statistic</p>
+                <div class="statistic-title-underline"></div>
+            </div>
+            <div class="specific-statistic container-fluid p-0">
+                <div class="row w-100 h-100 m-0">
+                    <div class="specific-statistic-courses col-sm-4 d-flex justify-content-center">
+                        <div class="item-name">
+                            Courses
+                            <div class="item-number">1,586</div>
+                        </div>                        
+                    </div>
+                    <div class="specific-statistic-lessons col-sm-4 d-flex justify-content-center">
+                        <div class="item-name">
+                            Lessons
+                            <div class="item-number">2,689</div>
+                        </div>                       
+                    </div>
+                    <div class="specific-statistic-learners col-sm-4 d-flex justify-content-center">
+                        <div class="item-name">
+                            Learners
+                            <div class="item-number">16,882</div>
+                        </div>                       
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="chat-box">
+            <div id="fb-root"></div>
+            <div id="fb-customer-chat" class="fb-customerchat"></div>
+        </div>
     </div>
-</div>
 @endsection
