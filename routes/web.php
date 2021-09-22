06bd86a9ae602eb8ad11fc78413bacaae7a4c1ed
hapo-tunght <tung.ht@haposoft.com>
2021-09-22 16:28:02 +0700

    fix convention file web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
